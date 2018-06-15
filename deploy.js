f6dfb2f3e5ca12aea73a57064592a374859ac4d6
kavija <karthickeyan.v03@gmail.com>
2018-06-15 14:56:36 -0500

    Add files via upload

---
+++ deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+ 'exhibit pride hold spy ask firm easy panther runway thumb one purpose',
+ 'https://ropsten.infura.io/UQXTaan7AZ5i9yU5aSga'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async() => {
+ const accounts = await web3.eth.getAccounts();
+  
+ console.log('Attempting to deploy from accounts', accounts[0]);
+
+ const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ from: accounts[0], gas: '1000000' });
+
+ console.log('Contract deployed to Address', result.options.address); 
+};
+deploy();
